395c53be37fc5ba33ef53e082456159264241c89
macrom1cro <macromicro37@gmail.com>
2025-10-28 01:41:51 +0300

    fix28

--- src/api/todosApi.ts
+++ src/api/todosApi.ts
@@ -1,18 +1,18 @@
+import { FilterStatus, SortingStatus } from "../components/Filters/Filters";
 import api from "./api";
 
 export const todosApi = {
   getTodos: (
     page: number = 1,
     limit: number = 5,
-    filter: "active" | "completed" | "all" = "all",
-    sorting: "newest" | "oldest" = "newest"
+    filter: FilterStatus = FilterStatus.ALL,
+    sorting: SortingStatus = SortingStatus.NEWEST
   ) =>
     api.get("/todos", {
-      params: { page, limit, filter, sorting }
+      params: { page, limit, filter, sorting },
     }),
   addTodo: (text: string) => api.post(`/todos`, { text }),
-  editTodo: (id: number, text: string) =>
-    api.put(`/todos/${id}`, { text}),
+  editTodo: (id: number, text: string) => api.put(`/todos/${id}`, { text }),
   editTodoCompleted: (id: number) => api.patch(`/todos/${id}/toggle`),
   deleteTodo: (id: number) => api.delete(`/todos/${id}`),
 };
